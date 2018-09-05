e4b07dc42f8bca70ef69c5360eb66d56ee4b8078
mvann <michaelmvann@gmail.com>
2018-09-05 15:59:02 -0700

    changed db.js to index.js

--- nedb-test/db/index.js
+++ nedb-test/db/index.js
@@ -1,8 +1,6 @@
 const Datastore = require('nedb');
 const path = require('path');
 
-console.log("dn: ", path.join(__dirname, 'collections'));
-
 class Database{
   constructor() {
     this.paths = new Datastore({ filename: path.join(__dirname, 'collections', 'projects'), autoload: true });
--- nedb-test/index.js
+++ nedb-test/index.js
@@ -1,4 +1,4 @@
-const db = require('./db/db.js');
+const db = require('./db');
 
 db.paths.find({}, (err, docs) => {
   if (err)
