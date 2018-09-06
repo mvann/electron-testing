128c364e181f3403986f0fb66132fe9a06353899
mvann <michaelmvann@gmail.com>
2018-09-05 18:54:53 -0700

    changed filename

--- nedb-test/db/index.js
+++ nedb-test/db/index.js
@@ -1,6 +1,6 @@
 const Datastore = require('nedb');
 const path = require('path');
-
+console.log(__dirname);
 class Database{
   constructor() {
     this.paths = new Datastore({ filename: path.join(__dirname, 'collections', 'projects'), autoload: true });
